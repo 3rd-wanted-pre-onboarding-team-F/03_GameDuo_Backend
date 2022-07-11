3d265c1e3b2e1e172c85bcd594beb61eda4ef87d
SMin1620 <ibb09@naver.com>
2022-07-11 16:43:44 +0900

    Feat : 모델 수정 Boss Raid 추가

--- bossRaid/admin.py
+++ bossRaid/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from bossRaid.models import (
     BossRaidHistory,
     BossRaidStatus,
+    BossRaid,
 )
 
 
 # Register your models here.
+admin.site.register(BossRaid)
 admin.site.register(BossRaidHistory)
 admin.site.register(BossRaidStatus)
 
--- bossRaid/models.py
+++ bossRaid/models.py
@@ -2,6 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+class BossRaid(models.Model):
+    name = models.CharField('보스 레이드 이름', max_length=100)
+
+    class Meta:
+        db_table = 'db_boss_raid'
+        ordering = ('-id',)
+        verbose_name = '보스 레이드'
+        verbose_name_plural = '보스 레이드들'
+
+    def __str__(self):
+        return f'{self.id}, {self.name}'
+
+
+
+
 class BossRaidHistory(models.Model):
     level = models.PositiveIntegerField('레벨')
     score = models.PositiveIntegerField('점수', default=0)
@@ -9,6 +24,7 @@ class BossRaidHistory(models.Model):
     end_time = models.DateTimeField('퇴장 시간', auto_now=True)
 
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    boss_raid = models.ForeignKey(BossRaid, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'tb_boss_history'
@@ -17,7 +33,7 @@ class BossRaidHistory(models.Model):
         verbose_name_plural = '보스 레이드 기록들'
 
     def __str__(self):
-        return f'{self.id}, level = {self.level}, score = {self.score}'
+        return f'{self.id}, level = {self.level}, score = {self.score} -- Boss Raid = {self.boss_raid}'
 
 
 class BossRaidStatus(models.Model):
@@ -25,8 +41,8 @@ class BossRaidStatus(models.Model):
     is_entered = models.BooleanField('입장 여부', default=False)
     last_entertime = models.DateTimeField('마지막 입장 시간', blank=True, auto_now=True)
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    boss_raid = models.ForeignKey(BossRaidHistory, on_delete=models.CASCADE)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    boss_raid = models.OneToOneField(BossRaid, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'tb_boss_status'
@@ -35,7 +51,7 @@ class BossRaidStatus(models.Model):
         verbose_name_plural = '보스 레이드 상태들'
 
     def __str__(self):
-        return f'{self.id}, level = {self.level}, is_Entered = {self.is_entered}'
+        return f'{self.id}, level = {self.level}, is_Entered = {self.is_entered} -- Boss Raid = {self.boss_raid}'
 
 
 
