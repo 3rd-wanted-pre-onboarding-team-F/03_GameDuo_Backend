19c0fb8d1dd29e81526943b0ff3b70d0293b39e9
Cat-Nile <imhyuk5054@gmail.com>
2022-07-12 13:35:20 +0900

    Feature: TotalScore 모델 추가

--- user/serializers.py
+++ user/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, TotalScore
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
         
+
+class TotalScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TotalScore
+        fields = ['id', 'user', 'total_score']
         
