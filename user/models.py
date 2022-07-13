f2fd73f1f7ddb48e695f380a82cb6778e2abbe52
SMin1620 <ibb09@naver.com>
2022-07-13 13:39:43 +0900

    Refactor - 보스레이드 시작 종료 수정 및 유저 수정

--- user/models.py
+++ user/models.py
@@ -1,17 +1,21 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-
 
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'users'
+
 
 class TotalScore(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    total_score = models.PositiveIntegerField(default=0)
-    
-@receiver(post_save, sender=User)
-def create_user_total_score(sender, instance, created, **kwargs):
-    if created:
-        TotalScore.objects.create(user=instance)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_score = models.IntegerField('총 합 점수', default=0)
+
+    class Meta:
+        db_table = 'tb_total_score'
+
+    def __str__(self):
+        return f'User = {self.user.username} --- Total Score = {self.total_score}'
