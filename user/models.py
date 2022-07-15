dddf9bab2dc0bba5de703776ced5f4d0534c4a57
Cat-Nile <imhyuk5054@gmail.com>
2022-07-15 09:53:05 +0900

    Docs - 코드 실명제 준수를 위한 주석 추가

--- user/models.py
+++ user/models.py
@@ -7,6 +7,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class TotalScore(models.Model):
+    """
+    author : 임혁, 이승민
+    explanation : User별 total_score(총 합 점수) 출력
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     total_score = models.IntegerField('총 합 점수', default=0)
 
@@ -19,5 +23,9 @@ class TotalScore(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_total_score(sender, instance, created, **kwargs):
+    """
+    author : 임혁
+    explanation : User 인스턴스 생성 시, post 시그널을 받아 TotalScore 인스턴스를 생성하는 함수
+    """
     if created:
         TotalScore.objects.create(user=instance)
--- user/serializers.py
+++ user/serializers.py
@@ -5,6 +5,10 @@ from .models import TotalScore
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    """
+    author : 임혁
+    explanation : 회원가입 관련 시리얼라이저
+    """
     password = serializers.CharField(
         write_only=True,
         required=True
@@ -15,6 +19,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('username', 'password')
 
     def create(self, validated_data):
+        """
+        author : 임혁
+        explanation : 회원가입 시 사용자 정보를 등록하는 함수
+        """
         user=User.objects.create_user(
             username=validated_data['username']
         )
@@ -24,6 +32,10 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.ModelSerializer):
+    """
+    author : 임혁
+    explanation : 로그인할 경우 필요한 필드 정보를 위한 시리얼라이저
+    """
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
@@ -32,6 +44,10 @@ class LoginSerializer(serializers.ModelSerializer):
         fields = ('username', 'password')
 
     def validate(self, data):
+        """
+        author : 임혁
+        explanation : 로그인 인증, 등록된 사용자가 맞는지 검증하는 함수
+        """
         user=authenticate(**data)
         if user:
             User.objects.get(username=user)
